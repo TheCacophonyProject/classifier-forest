d645fae1814e5c75db8a5b58b449746ea5cce959
GP <g.ferraro22@gmail.com>
2025-01-20 09:49:34 +1300

    add track level accuracy

--- validate.py
+++ validate.py
@@ -50,6 +50,8 @@ def main():
 
     fp_tags = ["water", "false-positive", "insect"]
     labels = ["animal", "false-positive", "vehicle"]
+    labels = ["animal", "false-positive"]
+
     ignore_labels = ["not identifiable", "other"]
     num_classes = len(labels)
     Y = []
@@ -80,6 +82,12 @@ def main():
     actual_classes = np.empty([0], dtype=int)
     predicted_classes = np.empty([0], dtype=int)
     predicted_prob = np.empty([0, num_classes])
+
+
+    track_actual_classes = np.empty([0], dtype=int)
+    track_predicted_classes = np.empty([0], dtype=int)
+    track_predicted_prob = np.empty([0, num_classes])
+    print("Num classes",num_classes)
     fold = 0
     X = np.array(X)
     Y = np.array(Y)
@@ -92,17 +100,31 @@ def main():
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = Y[train_index], Y[test_index]
 
+        group_test = groups[test_index]
+
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
         p_pred = model.predict_proba(
             X_test
         )  # Probabilities are useful for filtering and generating ROC curves
+        print(p_pred.shape)
+        track_prob, track_y, track_ids, track_predicted = track_accuracy(y_test,p_pred,group_test)
+        print(track_prob.shape,track_y.shape,track_predicted.shape)
+
+        track_actual_classes = np.append(track_actual_classes, track_y)
+        track_predicted_classes = np.append(track_predicted_classes, track_predicted)
+        track_predicted_prob = np.append(track_predicted_prob, track_prob, axis=0)
 
         actual_classes = np.append(actual_classes, y_test)
         predicted_classes = np.append(predicted_classes, y_pred)
         predicted_prob = np.append(predicted_prob, p_pred, axis=0)
 
     # Confusion matrix
+    print("Track level confusion")
+    print_confusion(labels, track_actual_classes, track_predicted_classes, track_predicted_prob, None)
+    print_confusion(labels, track_actual_classes, track_predicted_classes, track_predicted_prob, 0.7)
+
+    print("Frame level confusion")
     print_confusion(labels, actual_classes, predicted_classes, predicted_prob, None)
     print_confusion(labels, actual_classes, predicted_classes, predicted_prob, 0.7)
 
@@ -147,7 +169,28 @@ def main():
     # save
     joblib.dump(model, "model.pkl")
 
-
+def track_accuracy( actual_classes,predicted_probs,groups):
+    track_probs = {}
+    for y,prob,track_id in zip( actual_classes,predicted_probs,groups):
+        if track_id not in track_probs:
+            track_probs[track_id] ={"probs":[]}
+        
+        track_probs[track_id]["probs"].append(prob)
+        track_probs[track_id]["y"] = y
+        track_probs[track_id]["track_id"] = track_id
+    print("Got ", len(actual_classes), " this is ", len(track_probs), " tracks")
+    probs = []
+    tracks = []
+    track_y = []
+    track_predicted = []
+    for track_id, item in track_probs.items():
+        prob = np.mean(np.array(item["probs"]),axis=0)
+        probs.append(prob)
+        track_y.append(item["y"])
+        tracks.append(item["track_id"])
+        track_predicted.append(np.argmax(prob))
+    
+    return np.array(probs), np.array(track_y), np.array(tracks),np.array(track_predicted)
 def print_confusion(
     labels, actual_classes, predicted_classes, predicted_prob, threshold
 ):
